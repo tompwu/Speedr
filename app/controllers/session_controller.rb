9488f1bb2e2197b98c33fc65c1c0dfa844178c53
Tom <thomas.ulman@gmail.com>
2019-08-01 23:17:31 +1000

    fixed autoredirect on signup

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -5,7 +5,7 @@ class SessionController < ApplicationController
   def create
    user = User.find_by :username => params[:username]
      if user.present? && user.authenticate(params[:password])
-       session[:user_id] = user.id
+       session[:user_id] = @user.id
        #log em in
        redirect_to root_path
      else
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ before_action :authorise, :only => [:index]
   def create
     @user = User.new user_params
   if @user.save
+    session[:user_id] = @user.id
     redirect_to root_path
   else
     render :new
